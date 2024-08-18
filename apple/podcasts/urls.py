298e6139d53e9e46f510e650ab96e6ba50758b34
Souvik Naskar <shouvik0104@gmail.com>
2024-08-18 19:53:12 +0530

    podcasts episodes view, podcast link redirects to episodes list of that podcast

--- apple/podcasts/urls.py
+++ apple/podcasts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PodcastSearchView
+from .views import PodcastSearchView, PodcastEpisodesView
 
 
 urlpatterns = [
     path('search/', PodcastSearchView.as_view(), name='search-podcasts'),
+    path('<int:collection_id>/', PodcastEpisodesView.as_view(), name='episodes'),
 ]
--- apple/podcasts/views.py
+++ apple/podcasts/views.py
@@ -13,3 +13,19 @@ class PodcastSearchView(TemplateView):
             response = requests.get(url).json()
             context['response'] = response
         return context
+
+
+class PodcastEpisodesView(TemplateView):
+    template_name = 'podcasts/episodes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = kwargs.get('collection_id')
+        query = self.request.GET.get('query', '')
+        if query:
+            url = f'https://itunes.apple.com/lookup?id={collection_id}&entity=podcastEpisode&term={query}'
+            response = requests.get(url).json()
+            context['episodes'] = response.get('results', [])
+            context['collection_id'] = collection_id
+        return context
+
